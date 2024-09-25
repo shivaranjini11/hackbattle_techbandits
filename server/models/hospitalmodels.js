d6b97990d0225e0ebf25cff7f8ba7d5814e68b87
shivaranjini11 <shivaranjini.rameshkumar@gmail.com>
2024-09-25 23:19:28 +0530

    [Feat]:"hospital schema "

---
+++ server/models/hospitalmodels.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const hospitalSchema = Schema({
+    hospital_name:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        required: true
+    },
+    address:{
+        type: String,
+        required: false
+    }
+})
+
+module.exports = mongoose.model('hospital', hospitalSchema)
